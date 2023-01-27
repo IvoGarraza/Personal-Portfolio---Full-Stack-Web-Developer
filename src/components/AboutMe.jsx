e29f0bdeecba49848270c8e6fa9ad327bd24a279
ivo garraza <ivogarrachip@gmail.com>
2023-01-26 22:32:16 -0300

    about section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const AboutMe = () => {
   return (
     <div>
-        
+        <div>
+          <h2>Ivo Ezequiel Garraza</h2>
+          <h4>Full Stack Web Developer</h4>
+        </div>
+        <p>Estudiante de la Universidad  Tecnologica  Nacional  -  Facultad Regional Córdoba y Desarrolador Web Fullstack apasionado por la tecnologia y el  aprendizaje  para  el  desarrollo  personal  y profesional. Me gusta participar en proyectos donde los desafios intelectuales, el trabajo en equipo y las ganas sean fundamentales</p>
     </div>
   )
 }
