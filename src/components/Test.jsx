5ef48c01e90b1d97120a5477ccc71a0c2b5c3c38
ivo garraza <ivogarrachip@gmail.com>
2023-03-04 15:54:19 -0300

    cambio de tamaños de cards de projects

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -9,18 +9,18 @@ const Card = (props) => {
   const [isOpen, setOpen] = useState(false)
   return (
 
-    <motion.div onHoverStart={() => setOpen(true)} onHoverEnd={() => setOpen(false)} className={`${props.id} bg-center bg-no-repeat bg-cover bg-start m-2 flex flex-col items-center justify-center rounded-xl cursor-pointer h-[450px] w-[350px] `}>
+    <motion.div onHoverStart={() => setOpen(true)} onHoverEnd={() => setOpen(false)} className={`${props.id} bg-center bg-no-repeat bg-cover bg-start m-2 flex flex-col items-center justify-center rounded-xl cursor-pointer h-[450px] w-[95%] `}>
       {!isOpen &&
         (<motion.h2 className="text-white font-bold relative backdrop-blur-xl rounded-lg w-[90%] font-ubuntu"
           initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.2, delay: 0.1 }}>{props.title}</motion.h2>)}
       <AnimatePresence>
         {isOpen && (
-          <motion.div className="absolute p-4 h-[450px] w-[350px] flex flex-col items-center justify-center text-white bg-port__gray  rounded-xl font-ubuntu"
+          <motion.div className="absolute p-4 h-[450px] w-[24%] flex flex-col items-center justify-center text-white bg-port__gray  rounded-xl font-ubuntu"
             initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ duration: 0.2 }} exit={{ opacity: 0, scale: 0 }}>
             <p>
               {props.description}
             </p>
-            {props.link ? <a href={props.link} className='bg-purple-500 p-2 mt-5 w-[30%] rounded-lg text-white hover:bg-slate-300 hover:text-black bg-purple-500'>Link</a> : <Link to={props.path}><button className="bg-purple-500 p-2 mt-5 rounded-lg text-white hover:bg-slate-300 hover:text-black">Ver mas</button></Link>}
+            {props.link ? <a href={props.link} className='bg-purple-500 p-2 mt-5 w-[30%] rounded-lg text-white hover:bg-slate-300 hover:text-black'>Link</a> : <Link to={props.path}><button className="bg-purple-500 p-2 mt-5 rounded-lg text-white hover:bg-slate-300 hover:text-black">Ver mas</button></Link>}
             
             
           </motion.div>
